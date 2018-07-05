fcda518f7bd557241b28323a7978105ca1d82c78
Viktar Kurbatski <Viktar_Kurbatski@epam.com>
2018-07-05 17:13:20 +0300

    Add styles for Container component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
+
+// Styles
 import 'normalize.css';
 
+// Components
 import Container from './components/Container';
+
+// Images
+import containerBackground from './assets/img/app-bg.jpeg';
 
 class App extends Component {
   render() {
-    return <Container />;
+    return <Container backgroundPath={containerBackground} />;
   }
 }
 
--- src/components/Container.js
+++ src/components/Container.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import styled from 'styled-components';
 
-const ContainerWrapper = styled.div``;
+const ContainerWrapper = styled.div`
+  height: 100vh;
+  background: url(${props => props.backgroundPath}) no-repeat 50% / cover;
+`;
 
 const Container = props => {
-  return <ContainerWrapper>{props.children}</ContainerWrapper>;
+  return (
+    <ContainerWrapper backgroundPath={props.backgroundPath}>
+      {props.children}
+    </ContainerWrapper>
+  );
 };
 
 export default Container;
