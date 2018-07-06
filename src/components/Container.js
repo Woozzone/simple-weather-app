d5e463a3052bd87e489b3e0941b8f6788121af48
Viktar Kurbatski <Viktar_Kurbatski@epam.com>
2018-07-06 12:54:59 +0300

    Add center align

--- src/components/Container.js
+++ src/components/Container.js
@@ -4,6 +4,10 @@ import styled from 'styled-components';
 const ContainerWrapper = styled.div`
   height: 100vh;
   background: url(${props => props.backgroundPath}) no-repeat 50% / cover;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
 `;
 
 const Container = props => {
