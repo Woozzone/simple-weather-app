3007f8a379647192639d0747aebd5e6116d4c7cb
Viktar Kurbatski <Viktar_Kurbatski@epam.com>
2018-07-06 11:55:56 +0300

    Add default geolocation

--- src/containers/Weather.js
+++ src/containers/Weather.js
@@ -5,29 +5,44 @@ class Weather extends React.Component {
     super(props);
 
     this.state = {
-      isLoaded: false,
-      temperature: 0
+      isLoaded: false
     };
+
+    this.getCoords = this.getCoords.bind(this);
+  }
+
+  getCoords() {
+    return new Promise((resolve, reject) => {
+      navigator.geolocation.getCurrentPosition(resolve);
+    });
   }
 
   componentDidMount() {
-    fetch(
-      `https://api.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=${
-        process.env.REACT_APP_API_KEY
-      }`
-    )
-      .then(res => res.json())
-      .then(result => {
-        this.setState({
-          isLoaded: true,
-          temperature: result.main.temp
-        });
-      })
-      .catch(() => {
-        this.setState({
-          isLoaded: false
-        });
+    this.getCoords().then(position => {
+      this.setState({
+        latitude: Number.parseFloat(position.coords.latitude).toFixed(2),
+        longitude: Number.parseFloat(position.coords.longitude).toFixed(2)
       });
+
+      const url = `https://api.openweathermap.org/data/2.5/weather?lat=${
+        this.state.latitude
+      }&lon=${this.state.longitude}&appid=${
+        process.env.REACT_APP_API_KEY
+      }&units=metric`;
+      fetch(url)
+        .then(res => res.json())
+        .then(result => {
+          this.setState({
+            isLoaded: true,
+            temperature: result.main.temp
+          });
+        })
+        .catch(() => {
+          this.setState({
+            isLoaded: false
+          });
+        });
+    });
   }
 
   render() {
