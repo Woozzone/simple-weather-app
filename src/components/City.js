fb143e04f007bf263d62bccf05abcafb100b8154
Viktar Kurbatski <Viktar_Kurbatski@epam.com>
2018-07-06 13:53:48 +0300

    Change styles

--- src/components/City.js
+++ src/components/City.js
@@ -2,14 +2,16 @@ import React from 'react';
 import styled from 'styled-components';
 
 const CityWrapper = styled.div`
-  text-transform: uppercase;
-  font-size: 16px;
+  text-transform: capitalize;
+  font-size: 14px;
   font-family: 'Work Sans', sans-serif;
   color: #fff;
+  text-align: center;
+  margin-top: ${props => props.marginTop}px;
 `;
 
 const City = props => {
-  return <CityWrapper>{props.name}</CityWrapper>;
+  return <CityWrapper marginTop={props.marginTop}>{props.name}</CityWrapper>;
 };
 
 export default City;
