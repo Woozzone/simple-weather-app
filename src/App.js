451989bbb8c8d79430a6f89256b14cd3a6dfc97e
Viktar Kurbatski <Viktar_Kurbatski@epam.com>
2018-07-06 15:12:54 +0300

    Add weather-icons

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 
 // Styles
 import 'normalize.css';
+import 'weather-icons/css/weather-icons.css';
 
 // Components
 import Weather from './containers/Weather';
