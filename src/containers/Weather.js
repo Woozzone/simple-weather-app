97edfeb2f68826d13f4ea9e13ff8c167beea2200
Viktar Kurbatski <Viktar_Kurbatski@epam.com>
2018-07-07 14:44:41 +0300

    Add handler

--- src/containers/Weather.js
+++ src/containers/Weather.js
@@ -8,8 +8,9 @@ import City from '../components/City';
 import WeatherCondition from '../components/WeatherCondition';
 import CalcDate from '../components/CalcDate';
 import Stats from '../components/Stats';
+import HourlyWeather from '../components/HourlyWeather';
 
-const WeatherWrapper = styled.div`
+const CurrentWeather = styled.div`
   position: relative;
   min-width: 320px;
 `;
@@ -23,11 +24,22 @@ class Weather extends React.Component {
     };
 
     this.getCoords = this.getCoords.bind(this);
+    this.setCurrentWeather = this.setCurrentWeather.bind(this);
   }
 
   getCoords() {
     return new Promise((resolve, reject) => {
       navigator.geolocation.getCurrentPosition(resolve);
+    });
+  }
+
+  setCurrentWeather(index) {
+    this.setState({
+      temperature: Math.round(this.state.response.list[index].main.temp),
+      weatherCondition: this.state.response.list[index].weather,
+      date: this.state.response.list[index].dt,
+      humidity: this.state.response.list[index].main.humidity,
+      windSpeed: this.state.response.list[index].wind.speed
     });
   }
 
@@ -38,24 +50,24 @@ class Weather extends React.Component {
         longitude: Number.parseFloat(position.coords.longitude).toFixed(2)
       });
 
-      const url = `https://api.openweathermap.org/data/2.5/weather?lat=${
+      const url = `https://api.openweathermap.org/data/2.5/forecast?lat=${
         this.state.latitude
       }&lon=${this.state.longitude}&appid=${
         process.env.REACT_APP_API_KEY
       }&units=metric`;
-      console.log(url);
       fetch(url)
         .then(res => res.json())
         .then(result => {
-          console.log(result);
           this.setState({
+            response: result,
             isLoaded: true,
-            place: result.name,
-            temperature: Math.round(result.main.temp),
-            weatherCondition: result.weather,
-            date: result.dt,
-            humidity: result.main.humidity,
-            windSpeed: result.wind.speed
+            place: result.city.name,
+            temperature: Math.round(result.list[0].main.temp),
+            weatherCondition: result.list[0].weather,
+            date: result.list[0].dt,
+            humidity: result.list[0].main.humidity,
+            windSpeed: result.list[0].wind.speed,
+            list: result.list
           });
         })
         .catch(() => {
@@ -67,19 +79,29 @@ class Weather extends React.Component {
   }
 
   render() {
-    console.log(this.state);
     if (this.state.isLoaded) {
       return (
-        <WeatherWrapper>
-          <WeatherCondition value={this.state.weatherCondition} />
-          <Temperature value={this.state.temperature} />
-          <Stats
-            windSpeed={this.state.windSpeed}
-            humidity={this.state.humidity}
+        <div>
+          <CurrentWeather>
+            <WeatherCondition
+              value={this.state.weatherCondition}
+              fs={72}
+              description
+            />
+            <Temperature value={this.state.temperature} fs={72} />
+            <Stats
+              windSpeed={this.state.windSpeed}
+              humidity={this.state.humidity}
+            />
+            <CalcDate value={this.state.date} />
+            <City name={this.state.place} />
+          </CurrentWeather>
+
+          <HourlyWeather
+            onClick={this.setCurrentWeather}
+            list={this.state.list}
           />
-          <CalcDate value={this.state.date} />
-          <City name={this.state.place} />
-        </WeatherWrapper>
+        </div>
       );
     } else {
       return <Loader />;
