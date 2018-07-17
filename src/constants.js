96e00e0ef16f4a027ce986dff1c497782c4ef57a
Viktar Kurbatski <Viktar_Kurbatski@epam.com>
2018-07-17 16:19:25 +0300

    Add constants

---
+++ src/constants.js
@@ -0,0 +1,26 @@
+export const MONTHS = [
+  'January',
+  'February',
+  'March',
+  'April',
+  'May',
+  'June',
+  'July',
+  'August',
+  'September',
+  'October',
+  'November',
+  'December'
+];
+
+export const FULL_DAYS = [
+  'Sunday',
+  'Monday',
+  'Tuesday',
+  'Wednesday',
+  'Thursday',
+  'Friday',
+  'Saturday'
+];
+
+export const DAYS = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
