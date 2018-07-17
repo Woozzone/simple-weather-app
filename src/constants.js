6c7b291dab51be0a012fcf7c477822406abbb539
Viktar Kurbatski <Viktar_Kurbatski@epam.com>
2018-07-17 16:55:03 +0300

    Add weather icons

--- src/constants.js
+++ src/constants.js
@@ -24,3 +24,24 @@ export const FULL_DAYS = [
 ];
 
 export const DAYS = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+
+export const WEATHER_ICONS = {
+  '01d': 'wi-day-sunny',
+  '02d': 'wi-day-cloudy',
+  '03d': 'wi-cloud',
+  '04d': 'wi-cloudy',
+  '09d': 'wi-rain',
+  '10d': 'wi-day-rain',
+  '11d': 'wi-day-lightning',
+  '13d': 'wi-day-snow',
+  '50d': 'wi-fog',
+  '01n': 'wi-night-clear',
+  '02n': 'wi-night-alt-cloudy',
+  '03n': 'wi-cloud',
+  '04n': 'wi-cloudy',
+  '09n': 'wi-rain',
+  '10n': 'wi-night-alt-rain',
+  '11n': 'wi-night-alt-thunderstorm',
+  '13n': 'wi-night-snow',
+  '50n': 'wi-fog'
+};
